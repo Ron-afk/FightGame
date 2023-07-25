7a67194344f211ca33b00e8adeec818848dc3f64
Ron-afk <robbie616318821@gmail.com>
2023-07-24 20:50:10 -0700

    Partial animation added

---
+++ js/classes.js
@@ -0,0 +1,117 @@
+class Sprite{
+    constructor({position, imageSrc, scale=1, framesMax = 1, framesHold=20,offset={x:0,y:0}}){
+        this.position = position
+        this.width = 50
+        this.height = 150
+        this.image = new Image()
+        this.image.src = imageSrc
+        this.scale = scale
+        this.framesMax = framesMax
+        this.frameCurrent = 0
+        this.framesElapsed = 0
+        this.framesHold = 20
+        this.offset = offset
+    }
+
+    draw(){
+        c.drawImage(
+            this.image, 
+            this.frameCurrent * (this.image.width / this.framesMax),
+            0,
+            this.image.width/this.framesMax,
+            this.image.height,
+            this.position.x - this.offset.x,
+            this.position.y - this.offset.y,
+            this.image.width/this.framesMax * this.scale, 
+            this.image.height * this.scale
+        )
+    }
+
+    animateFrames(){
+        this.framesElapsed ++
+        if(this.framesElapsed % this.framesHold === 0) {
+            if(this.frameCurrent < this.framesMax -1) {
+                this.frameCurrent++
+            } else {
+                this.frameCurrent = 0;
+            }
+        }  
+    }
+
+    update(){
+        this.draw()
+        this.animateFrames()
+    }
+}
+
+class Fighter extends Sprite{
+    constructor({position,
+         velocity, 
+         color='red',
+         imageSrc, 
+         scale=1, 
+         framesMax = 1,
+         framesHold=20,
+         offset={x:0,y:0},
+         sprites
+        }) {
+            super({
+                position,
+                imageSrc,
+                scale,
+                framesMax,
+                framesHold,
+                offset
+            })
+            this.position = position
+            this.velocity = velocity
+            this.width = 50
+            this.height = 150
+            this.lastKey
+            this.attackBox = {
+                position: {
+                    x: this.position.x,
+                    y: this.position.y
+                },
+                offset,
+                width: 100,
+                height: 50
+            }
+            this.color = color
+            this.isAttacking
+            this.health=100
+            this.frameCurrent=0
+            this.framesElapsed = 0
+            this.sprites = sprites
+            this.framesHold = framesHold
+
+            for (const sprite in this.sprites){
+                this.sprites[sprite].image = new Image()
+                this.sprites[sprite].image.src = sprites[sprite].imageSrc 
+            }
+            console.log(this.sprites)
+    }
+
+
+    update(){
+        this.draw()
+        this.animateFrames()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        if(this.position.y + this.height  + this.velocity.y >= canvas.height-95) {
+            this.velocity.y = 0
+        } else {
+            this.velocity.y += gravity
+        }
+    }
+
+    attack(){
+        this.isAttacking = true
+        setTimeout(() =>{
+            this.isAttacking = false
+        },100)
+    }
+}
